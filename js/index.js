9f809383549a0d14cbfc01291dc2c0256a7e58b6
md8-habibullah <md8.habibullah@gmail.com>
2024-10-09 11:28:53 +0600

    problem

---
+++ js/index.js
@@ -0,0 +1,18 @@
+console.log("ijii");
+// fetch("https://openapi.programming-hero.com/api/peddy/categories")
+//   .then((response) => {
+//     response.json()
+//   })
+//   .then((data) => {
+//     console.log(data); // fetched data
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching data:", error); //  errors here
+//   });
+
+//   const displayCategorys = () => {
+    
+//   }
+
+
+//   displayCategorys()
